14e9cb2f64f39e8afb19369b4422d65bd657e847
SuDin neUpaNe <160850764+Sudin-Neupane@users.noreply.github.com>
2026-01-03 19:27:36 +0545

    Fix sizeof() usage in variable size printing
    Corrected the use of sizeof() in scanf and printf statements.

---
+++ use of sizeof().cpp
@@ -0,0 +1,11 @@
+//23. C progrom to print size of variables  using size of () operator 
+#include<stdio.h>
+int main()
+{
+	int x;
+	float y; 
+	char c;
+	scanf("%d\t %d\t %d\t",sizeof(x),sizeof(y),sizeof (z));
+	printf("\n%d\t %d\t %d\t ",size of(int),size of(float),size of(char));
+	return 0;
+}
