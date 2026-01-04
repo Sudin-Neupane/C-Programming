65da271d2e86b5ea4b1329cb28f5e06811da98c5
SuDin neUpaNe <160850764+Sudin-Neupane@users.noreply.github.com>
2026-01-04 21:31:50 +0545

    Add Fibonacci series implementation in C

---
+++ Fibonacci series.cpp
@@ -0,0 +1,18 @@
+//Fibonacci series
+
+#include<stdio.h>
+int main()
+{
+int n,a=0,b=1,c,i;
+printf("Enter a number");
+scanf("%d",&n);
+printf("%d%d",a,b);
+for(i=3;i<=n;i++)
+{
+c=a+b;
+printf("%d",c);
+a=b;
+b=c;
+}
+return 0;
+}
