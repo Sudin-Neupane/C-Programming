2382077ee9e2ecb82fa75b0c49dc65024a560a8f
SuDin neUpaNe <160850764+Sudin-Neupane@users.noreply.github.com>
2026-01-16 21:11:08 +0545

    Create ascii.cpp

---
+++ ascii.cpp
@@ -0,0 +1,14 @@
+// Print ASCII value 
+
+#include <stdio.h>
+
+int main() {
+    char ch;
+
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+
+    printf("ASCII value of '%c' is %d\n", ch, ch);
+
+    return 0;
+}
