ec5be7ae0770a8d58d8ba8782a94f5fa08a616fe
SuDin neUpaNe <160850764+Sudin-Neupane@users.noreply.github.com>
2026-01-19 10:55:14 +0545

    Create $#$#$ format.cpp

---
+++ $#$#$ format.cpp
@@ -0,0 +1,28 @@
+/*
+print in this format 
+$ # $ # $
+$ # $ # $
+$ # $ # $
+
+*/ 
+
+#include <stdio.h>
+
+int main()
+{
+    int i, j;
+
+    for(i = 1; i <= 3; i++)
+    {
+        for(j = 1; j <= 5; j++)
+        {
+            if(j % 2 == 1)
+                printf("$ ");
+            else
+                printf("# ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
