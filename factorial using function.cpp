5904b628a5bdd027e6b039df4aa611c827bddbbb
SuDin neUpaNe <160850764+Sudin-Neupane@users.noreply.github.com>
2026-01-31 23:41:46 +0545

    Add files via upload

---
+++ factorial using function.cpp
@@ -0,0 +1,21 @@
+// WAP to find factorial of a number using function passing argurment and taking return value
+
+#include<stdio.h>
+int fact(int);
+int main(){
+	int factorial , res,a;
+	printf("Enter a number");
+	scanf("%d",&a);
+	res=fact(a);
+	printf("Factorial is %d",res);
+	return 0;
+}
+int fact(int x){
+	
+	int fact=1;
+	int i;
+	for(i=1;i<x;i++){
+		fact=fact*i;
+	}
+	return fact;
+}
