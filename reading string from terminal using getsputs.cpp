f14e6e0a9236f8973b66738101ac6026421c293d
SuDin neUpaNe <160850764+Sudin-Neupane@users.noreply.github.com>
2026-02-04 11:24:21 +0545

    Add files via upload

---
+++ reading string from terminal using getsputs.cpp
@@ -0,0 +1,12 @@
+//// reading string form terminal keyboard using gets , puts
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+	char name[15];
+	printf("Enter");
+	gets(name);
+	printf("name");
+	puts(name);
+	return 0;
+}
