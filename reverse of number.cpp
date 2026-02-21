4a6f252dc6acd22138bf2331acbc026b1b677899
SuDin neUpaNe <160850764+Sudin-Neupane@users.noreply.github.com>
2026-02-21 18:18:20 +0545

    Add program to reverse a number

---
+++ reverse of number.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+int num,rev=0,rem;
+
+printf("Enter a number: ");
+scanf("%d",&num);
+
+while(num!=0){
+rem=num%10;
+rev=rev*10+rem;
+num=num/10;
+}
+
+printf("Reversed number: %d",rev);
+
+return 0;
+}
