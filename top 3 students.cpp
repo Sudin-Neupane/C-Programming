47e881cbcd961926849f17a40d24af2a803ce1ac
SuDin neUpaNe <160850764+Sudin-Neupane@users.noreply.github.com>
2026-03-15 23:55:45 +0545

    Add program to find student with highest marks
    Implement a program to store and display details of 3 students.

---
+++ top 3 students.cpp
@@ -0,0 +1,32 @@
+//Question: Write a program using structure to store the details of 3 students and display the names of students who scored the highest marks.
+
+#include<stdio.h>
+
+struct Student{
+    int roll;
+    char name[50];
+    float marks;
+};
+
+int main(){
+    struct Student s[3];
+    int i,maxIndex=0;
+
+    for(i=0;i<3;i++){
+        printf("Enter roll, name and marks of student %d: ",i+1);
+        scanf("%d %s %f",&s[i].roll,s[i].name,&s[i].marks);
+    }
+
+    for(i=1;i<3;i++){
+        if(s[i].marks > s[maxIndex].marks){
+            maxIndex = i;
+        }
+    }
+
+    printf("\nStudent with Highest Marks:\n");
+    printf("Roll: %d\n",s[maxIndex].roll);
+    printf("Name: %s\n",s[maxIndex].name);
+    printf("Marks: %.2f\n",s[maxIndex].marks);
+
+    return 0;
+}
