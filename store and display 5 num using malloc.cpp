07a375741fa4649e8d9fef88a266a60aecd2800e
SuDin neUpaNe <160850764+Sudin-Neupane@users.noreply.github.com>
2026-05-19 17:59:00 +0545

    Create store and display 5 num using malloc.cpp

---
+++ store and display 5 num using malloc.cpp
@@ -0,0 +1,20 @@
+
+// C program to store and display 5 numbers using malloc 
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+int *ptr,i;
+ptr = (int*)malloc(5*sizeof(int));
+printf("Enter 5 numbers:\n");
+for(i=0;i<5;i++){
+scanf("%d",&ptr[i]);
+}
+printf("Entered numbers are:\n");
+for(i=0;i<5;i++){
+printf("%d ",ptr[i]);
+}
+
+free(ptr);
+
+return 0;
+}
